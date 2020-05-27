7853996de0209be719500dac84aa09a6f503688c
MikeTheMarketer <65884641+MikeTheMarketer@users.noreply.github.com>
2020-05-26 21:40:16 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+week = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+
+fruits = set(["apple", "mango", "orange"])
+
+new_fruits = set(["cherry", "peach", "apple", "mango"])
+
+print(new_fruits.difference(fruits))
+print('\n')
+
+print(new_fruits.intersection(fruits))
